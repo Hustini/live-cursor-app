6ed7c22cd9f1cd8b3ed4f06bae58f13a1c00ea8c
Hustini <ejdersalih02@gmail.com>
2025-02-12 16:18:13 +0100

    able to username and generate uuid

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const http = require("http");
 const {WebSocketServer} = require("ws");
 
+const url = require("url");
+const uuidv4 = require("uuid").v4;
+
 const server = http.createServer();
 const wsServer = new WebSocketServer({server});
 const port = 8080;
+
+wsServer.on("connection", (connection, request) => {
+    // ws://localhost:8080?username=Alex
+
+    const {username} = url.parse(request.url,true).query
+    const uuid = uuidv4()
+    console.log(username);
+    console.log(uuid);
+})
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
