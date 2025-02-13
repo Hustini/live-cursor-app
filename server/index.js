95f5d3e93e7ce541c49994be10943663d32c58e0
Hustini <ejdersalih02@gmail.com>
2025-02-13 10:49:52 +0100

    implemented broadcastUSers func

--- server/index.js
+++ server/index.js
@@ -11,8 +11,12 @@ const port = 8080;
 const connections = {}
 const users = {}
 
-const broadcast = () => {
-
+const broadcastUsers = () => {
+    Object.keys(users).forEach(uuid => {
+        const connection = connections[uuid];
+        const message = JSON.stringify(users);
+        connection.send(message)
+    })
 }
 
 const handleMessage = (bytes, uuid) => {
@@ -20,6 +24,9 @@ const handleMessage = (bytes, uuid) => {
     const message  = JSON.parse(bytes.toString())
     const user = users[uuid];
     user.state = message
+
+    broadcastUsers()
+
     console.log(message)
 }
 
