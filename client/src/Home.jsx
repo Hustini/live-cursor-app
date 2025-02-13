7d5c1c85f6b96787139ea6ce26f7ea1e171415e7
Hustini <ejdersalih02@gmail.com>
2025-02-13 17:27:39 +0100

    connected react frontend with the server, throttled the move mouse event listener and send x and y coords

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,4 +1,26 @@
+import useWebSocket from "react-use-websocket";
+import {useEffect, useRef} from "react";
+import throttle from 'lodash.throttle'
+
 export function Home({username}) {
+
+    const WS_URL='ws://127.0.0.1:8080'
+    const {sendJsonMessage} = useWebSocket(WS_URL, {
+        queryParams: {username}
+    });
+
+    const THROTTLE = 50
+    const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE))
+
+    useEffect(() => {
+        window.addEventListener("mousemove", (e) => {
+            sendJsonMessageThrottled.current({
+                x: e.clientX,
+                y: e.clientY
+            })
+        })
+    }, [])
+
     return (
         <h1>Hello, {username}</h1>
     )
