cde1cad7eb6083aa59650cc19d6c3a07f8fa1b59
Hustini <ejdersalih02@gmail.com>
2025-02-13 16:54:11 +0100

    added login component

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -1,7 +1,27 @@
-import { useState} from "react";
+import {useState} from "react";
 
 export function Login() {
+    const [username, setUsername] = useState("");
     return (
-        <div className="login">Hello</div>
+        <>
+            <h1>Welcome</h1>
+            <p>What should you be called</p>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault()
+                    onSubmit(username)
+                }}
+            >
+                <input
+                    type="text"
+                    value={username}
+                    placeholder="username"
+                    onChange={(e) => {
+                        e.target.value
+                    }}
+                />
+                <input type="submit"/>
+            </form>
+        </>
     )
 }
